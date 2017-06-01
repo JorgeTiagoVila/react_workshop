951fb5a0b0ec061afe910d5f47d87ca47b6b5b44
Jorge Tiago Vila <jorge.vila@elecctro.com>
2017-06-01 11:54:43 +0100

    Created the Users container.

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,33 +1,35 @@
 import React, { Component } from 'react';
 
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import { addUser } from '../actions/users';
+
 import Form from './Form';
 import Table from './Table';
 
 class Users extends Component {
 
-    state = {
-        users: []
-    };
-
-    handleSubmit = (firstName, lastName) => {
-        this.setState((state) => {
-            state.users.push({
-                firstName,
-                lastName
-            });
-
-            return state;
-        });
-    };
-
     render() {
         return (
             <div>
-                <Form onSubmit={this.handleSubmit} />
-                <Table users={this.state.users} />
+                <Form onSubmit={this.props.actions.addUser} />
+                <Table users={this.props.users} />
             </div>
         );
     }
 }
 
-export default Users;
+const mapStateToProps = (state) => ({
+    users: state.users
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    actions: bindActionCreators({
+        addUser
+    }, dispatch)
+});
+
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
+
+export default UsersContainer;
