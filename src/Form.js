0d04f5ce9a21b81effdf9ea83911b9ffdaab5d6f
Jorge Tiago Vila <jorge.vila@elecctro.com>
2017-06-01 10:42:24 +0100

    Made form inputs controlled.

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+    state = {
+        firstName: '',
+        lastName: ''
+    };
+
     render() {
         return (
             <form>
-                <input type="text" placeholder="First Name" />
-                <input type="text" placeholder="Last Name" />
+                <input type="text"
+                       placeholder="First Name"
+                       value={this.state.firstName}
+                       onChange={(event) => this.setState({ firstName: event.target.value })}
+                />
+                <input type="text"
+                       placeholder="Last Name"
+                       value={this.state.lastName}
+                       onChange={(event) => this.setState({ lastName: event.target.value })}
+                />
                 <input type="submit" />
             </form>
         );
